2d74865ad3245cd3214f1fe4f43affa7b14e7764
dev-vue <dev.kittipich@gmail.com>
2025-07-29 14:19:03 +0700

    Refactor layout and structure of HomePage and MainLayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,109 +73,118 @@ export default function HomePage() {
   if (isLoading) return <Loading fullscreen />
 
   return (
-    <>
-      <HeroSlider banners={data?.bannerList ?? []} />
+    <main className=''>
+      {/* lg:container mx-auto space-y-8 py-5 md:px-5 px-3 */}
+      <div className='bg-white'>
+        <div className='lg:container mx-auto space-y-8 py-5 md:px-5 px-3 '>
+          <HeroSlider banners={data?.bannerList ?? []} />
 
-      {/* Categories Section */}
-      <section>
-        <h3 className="text-2xl font-semibold mb-4">หมวดหมู่สินค้า</h3>
-        <div className="relative">
-          <Swiper
-            spaceBetween={16}
-            slidesPerView={CATEGORIES.length === 1 ? 1 : 1.5}
-            breakpoints={{
-              768: {
-                slidesPerView: CATEGORIES.length === 1 ? 1 : 2,
-                spaceBetween: 24,
-              },
-              1024: {
-                slidesPerView: CATEGORIES.length === 1 ? 1 : 2,
-                spaceBetween: 24,
-              }
-            }}
-            modules={[Navigation, Pagination]}
-            className="categories-swiper"
-          >
-            {data?.animalCategoryList?.map((category, index) => (
-              <SwiperSlide key={index}>
-                <div className="relative rounded-lg overflow-hidden lg:h-[377px] md:h-[172px] h-[118px]"
-                  onClick={() => SearchCategory(category.name, category.animalType)}
-                >
-                  <Image
-                    src={category.imageUrl}
-                    alt={category.name}
-                    layout="fill"
-                    objectFit="cover"
-                    className='cursor-pointer'
-
-                  />
-                  {/* <div className="absolute inset-0 bg-black bg-opacity-30"></div> */}
-                  {/* Desktop & Tablet Button - Hidden on Mobile */}
-                  <div className="absolute inset-0  items-end p-4 lg:flex hidden">
-                    <button className="bg-white text-black font-semibold py-2 px-4 rounded-full"
+          {/* Categories Section */}
+          <section>
+            <h3 className="text-2xl font-semibold mb-4">หมวดหมู่สินค้า</h3>
+            <div className="relative">
+              <Swiper
+                spaceBetween={16}
+                slidesPerView={CATEGORIES.length === 1 ? 1 : 1.5}
+                breakpoints={{
+                  768: {
+                    slidesPerView: CATEGORIES.length === 1 ? 1 : 2,
+                    spaceBetween: 24,
+                  },
+                  1024: {
+                    slidesPerView: CATEGORIES.length === 1 ? 1 : 2,
+                    spaceBetween: 24,
+                  }
+                }}
+                modules={[Navigation, Pagination]}
+                className="categories-swiper"
+              >
+                {data?.animalCategoryList?.map((category, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="relative rounded-lg overflow-hidden lg:h-[377px] md:h-[172px] h-[118px]"
                       onClick={() => SearchCategory(category.name, category.animalType)}
                     >
-                      ดูสินค้า{category.name}
-                    </button>
-                  </div>
-                </div>
-                {/* Mobile- Hidden on Desktop & Tablet Button */}
-                <div className="bg-opacity-30 items-end py-4 lg:hidden flex">
-                  <button
-                    type='button'
-                    className=" text-black font-semibold py-2 px-4 rounded-full flex gap-x-2 items-center"
-                    onClick={() => SearchCategory(category.name, category.animalType)}
-                  >
-                    ดูสินค้า{category.name} <ChevronRight className='w-4 h-4' />
-                  </button>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </section>
+                      <Image
+                        src={category.imageUrl}
+                        alt={category.name}
+                        layout="fill"
+                        objectFit="cover"
+                        className='cursor-pointer'
 
-      {/* Products Section */}
-      <section>
-        <h3 className="text-2xl font-semibold mb-4">รายการสินค้าทั้งหมด</h3>
-        <div className="flex space-x-2 mb-6 overflow-x-auto pb-2">
-          {filters.map((filter, index) => (
-            <button
-              key={index}
-              className={
-                "cursor-pointer px-4 py-2 rounded-full whitespace-nowrap border " +
-                (activeFilter === filter.value
-                  ? "bg-primary-light text-primary border-primary"
-                  : "bg-white border-gray-300 hover:bg-gray-200")
+                      />
+                      {/* <div className="absolute inset-0 bg-black bg-opacity-30"></div> */}
+                      {/* Desktop & Tablet Button - Hidden on Mobile */}
+                      <div className="absolute inset-0  items-end p-4 lg:flex hidden">
+                        <button className="bg-white text-black font-semibold py-2 px-4 rounded-full"
+                          onClick={() => SearchCategory(category.name, category.animalType)}
+                        >
+                          ดูสินค้า{category.name}
+                        </button>
+                      </div>
+                    </div>
+                    {/* Mobile- Hidden on Desktop & Tablet Button */}
+                    <div className="bg-opacity-30 items-end py-4 lg:hidden flex">
+                      <button
+                        type='button'
+                        className=" text-black font-semibold py-2 px-4 rounded-full flex gap-x-2 items-center"
+                        onClick={() => SearchCategory(category.name, category.animalType)}
+                      >
+                        ดูสินค้า{category.name} <ChevronRight className='w-4 h-4' />
+                      </button>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </section>
+        </div>
+      </div>
+
+      <div className='bg-gray-light'>
+        <div className='lg:container mx-auto space-y-8 py-5 md:px-5 px-3 '>
+          <section>
+            <h3 className="text-2xl font-semibold mb-4">รายการสินค้าทั้งหมด</h3>
+            <div className="flex space-x-2 mb-6 overflow-x-auto pb-2">
+              {filters.map((filter, index) => (
+                <button
+                  key={index}
+                  className={
+                    "cursor-pointer px-4 py-2 rounded-full whitespace-nowrap border " +
+                    (activeFilter === filter.value
+                      ? "bg-primary-light text-primary border-primary"
+                      : "bg-white border-gray-300 hover:bg-gray-200")
+                  }
+                  onClick={() => setActiveFilter(filter.value)}
+                  type="button"
+                >
+                  {filter.label}
+                </button>
+              ))}
+            </div>
+
+            {/* Section Product List */}
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+              {
+                productsLoading ? <Loading className='w-full col-span-4' /> :
+                  allProducts.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                  ))
               }
-              onClick={() => setActiveFilter(filter.value)}
-              type="button"
-            >
-              {filter.label}
-            </button>
-          ))}
+            </div>
+
+            {/* Intersection Observer Target */}
+            <div ref={loadMoreRef} className="h-10 w-full" />
+
+            {/* Load More Indicator */}
+            {isFetchingNextPage && (
+              <div className="flex justify-center mt-6 w-full">
+                <Loading className='w-full' />
+              </div>
+            )}
+          </section>
         </div>
+      </div>
 
-        {/* Section Product List */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {
-            productsLoading ? <Loading className='w-full col-span-4' /> :
-              allProducts.map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))
-          }
-        </div>
-
-        {/* Intersection Observer Target */}
-        <div ref={loadMoreRef} className="h-10 w-full" />
-
-        {/* Load More Indicator */}
-        {isFetchingNextPage && (
-          <div className="flex justify-center mt-6 w-full">
-            <Loading className='w-full' />
-          </div>
-        )}
-      </section>
 
       <SearchCategoryModal
         open={categorySearchModal}
@@ -183,6 +192,6 @@ export default function HomePage() {
         name={categoryFilter.name}
         onClose={() => setCategorySearchModal(false)}
       />
-    </>
+    </main>
   );
 }
--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -7,10 +7,10 @@ export type LayoutsProps = {
 
 export default function MainLayout({ children }: LayoutsProps) {
     return (
-        <div className="bg-gray-100 min-h-screen font-sans">
-            <main className="lg:container mx-auto space-y-8 py-5 md:px-5 px-3">
-                {children}
-            </main>
+        <div className="min-h-screen font-sans">
+            {/* <main className="lg:container mx-auto space-y-8 py-5 md:px-5 px-3"> */}
+            {children}
+            {/* </main> */}
         </div>
     );
 }
